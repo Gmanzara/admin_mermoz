b9fdc513525a1c39c37ac8260f8e6e343d391d9c
GBANE MANZARA <mgbane@CNTIG-PC302.CNTIG.CI>
2023-03-17 14:48:27 +0000

    first commit

---
+++ admin_mermoz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace admin_mermoz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
